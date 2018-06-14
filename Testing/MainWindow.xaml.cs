8de28a8de591596a832c97d9db3f4f0288317feb
mordashov <mordashovv@gmail.com>
2018-06-14 15:17:20 +0300

    Генерация и получения ответа от радиокнопок

--- Testing/MainWindow.xaml.cs
+++ Testing/MainWindow.xaml.cs
@@ -23,6 +23,35 @@ namespace Testing
         public MainWindow()
         {
             InitializeComponent();
+
+            for (int i = 0; i < 4; i++)
+            {
+                RadioButton rb = new RadioButton()
+                    {Content = "Radio button " + i, IsChecked = i == 0};
+                rb.Checked += (sender, args) => { Console.WriteLine("Pressed " + (sender as RadioButton).Tag); };
+                rb.Unchecked += (sender, args) => { };
+                rb.Tag = i;
+                StackPanelAnswers.Children.Add(rb);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string txt = "";
+            foreach (var item in StackPanelAnswers.Children)
+            {
+                if (item is RadioButton)
+                {
+                    RadioButton rb = (RadioButton) item;
+                    if (rb.IsChecked == true)
+                    {
+                        txt = rb.Content.ToString();
+                        MessageBox.Show(txt);
+                        break;
+                    }
+                }
+               
+            }
         }
     }
 }
